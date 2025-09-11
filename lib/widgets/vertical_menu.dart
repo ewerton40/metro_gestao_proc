ad38c2063c26c8526af226c216782aa15ca563b7
EWERTON DA CRUZ PEREIRA <24.10113-1@maua.br>
2025-09-11 15:16:34 -0300

    feat: adding the structure of vertical menu

---
+++ lib/widgets/vertical_menu.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+
+class VerticalMenu extends StatefulWidget{
+    const VerticalMenu({super.key});
+
+    @override
+    State<VerticalMenu> createState() => _VerticalMenuState();
+
+}
+
+
+class _VerticalMenuState extends State<VerticalMenu>{
+
+    @override
+    Widget build(BuildContext context){
+        return  
+Drawer(
+         child: ListView(
+            padding: EdgeInsets.zero,
+            children:  <Widget>[
+                SizedBox(
+                    height: 130,
+                    child: DrawerHeader(
+                            child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                    SizedBox(
+                                    width: 50,
+                                    height: 50,
+                                    child:  Image.asset('assets/images/logo_metro_menu.png')
+                                    ),    
+                                                        
+                                ],
+                            )        
+                ),
+                ),
+                ListTile(
+                    leading: const Icon(Icons.home),
+                    title: const Text('Início'),
+                    onTap:() {
+                      Navigator.pop(context);
+                    },
+                )
+
+            ],
+         )
+        );
+    }
+}
