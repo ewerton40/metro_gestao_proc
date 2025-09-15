b10fcde123ca62f294cdeca8b53193eb3090be42
Ewerton <ewertoncruzds@gmail.com>
2025-09-14 23:26:47 -0300

    style: adjusts in the menu bar

--- lib/widgets/bar_menu.dart
+++ lib/widgets/bar_menu.dart
@@ -26,13 +26,42 @@ class _BarMenuState extends State<BarMenu>{
         ),
       ),
       actions: [
-        IconButton(
+        Padding(
+          padding: const EdgeInsets.only(right: 50 ),
+          child: IconButton(
           onPressed: (){},
           icon: const Icon(Icons.notifications),
         ),
-        IconButton(
-          onPressed: (){}, 
-          icon: const Icon(Icons.person))
+        ),
+        Padding(
+              padding: const EdgeInsets.only(right: 50),
+            child: PopupMenuButton(
+              child: TextButton.icon(
+              onPressed: null,
+              icon: const Icon(Icons.person, color: Colors.black,),
+              label: Text('User', style: TextStyle(color: Colors.black)),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                foregroundColor: Colors.black
+                )
+          ),
+          offset: const Offset(0, 50),
+          itemBuilder:(BuildContext context) => <PopupMenuEntry<String>>[
+           PopupMenuItem(
+            child: TextButton.icon(
+              onPressed: null,
+              icon: Icon(Icons.settings),
+              label: const Text('Configurações', style: TextStyle(color: Colors.black),),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.transparent,
+                foregroundColor: Colors.black
+                )
+              ) 
+              ),
+
+          ]
+          ),
+          ),
       ],
     );
     
